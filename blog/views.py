28eb4adf35c339aae6db467bd8a18fbe689f5467
Уланов Роман <gogon1995@mail.ru>
2023-08-17 19:30:39 +0400

    prefetch posts count for tags

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 from django.contrib.auth.models import User
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 
 
 def get_related_posts_count(tag):
@@ -37,7 +37,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(posts_count=Count('posts'))],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -46,10 +46,15 @@ def serialize_tag(tag):
     
     return {
         'title': tag.title,
-        #'posts_with_tag': Post.objects.annotate(Count("tags")),
         'posts_with_tag':tag.posts_count
+    }
 
-        #'posts_with_tag': len(Post.objects.filter(tags=tag)),
+
+def serialize_optimize_tag(tag):
+    
+    return {
+        'title': tag.title,
+        'posts_with_tag':tag.posts_count
     }
 
 
@@ -68,9 +73,10 @@ def fetch_with_comments_count(posts):
 
 def index(request):
 
-    most_popular_posts = fetch_with_comments_count(Post.objects.popular().prefetch_related('author','tags')[:5])
+    most_popular_posts = fetch_with_comments_count(Post.objects.popular().prefetch_related('author',Prefetch('tags', queryset=Tag.objects.all().annotate(posts_count=Count('posts'))))[:5])
     
-    fresh_posts = Post.objects.prefetch_related('author','tags').annotate(comments_count=Count('comments', distinct=True)).order_by('published_at')
+    fresh_posts = Post.objects.prefetch_related('author',
+                                                Prefetch('tags', queryset=Tag.objects.all().annotate(posts_count=Count('posts')))).annotate(comments_count=Count('comments', distinct=True)).order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
     most_popular_tags = Tag.objects.popular().annotate(posts_count=Count('posts'))[:5]
     
